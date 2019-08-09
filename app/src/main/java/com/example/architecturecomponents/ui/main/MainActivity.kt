8fc0297daaa8a4088824e4f4be226fbfbdf6ef58
Rifat <rifat@setuptech.international>
2019-08-09 16:30:28 +0600

    Add log methods

--- app/src/main/java/com/example/architecturecomponents/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/architecturecomponents/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.architecturecomponents.ui.main
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import com.example.architecturecomponents.R
@@ -9,10 +11,12 @@ import com.example.architecturecomponents.data.LoggingViewModel
 import com.example.architecturecomponents.data.LoggingViewModelFactory
 import com.example.architecturecomponents.ui.setRipple
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private var viewModel: LoggingViewModel? = null
+    private val LOG_TAG = this.javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +30,31 @@ class MainActivity : AppCompatActivity() {
 
         text_view_click.setOnClickListener {
             viewModel?.count = viewModel?.count?.inc()!!
-            text_view_click.text = (viewModel?.count).toString()
+
+            // Here is the debug log message
+            Log.w(LOG_TAG, "The value is: $Random.nextInt(0, 9). Is it between 0 and 9?")
+
+            val randomValue = Random.nextInt(0, 9)
+
+            // Here is the warning log message
+            Log.w(LOG_TAG, "The value is: $randomValue. (It should not be zero)")
+
+            try {
+                viewModel?.count = viewModel?.count!! / randomValue
+                text_view_click.text = (viewModel?.count).toString()
+                Log.i(LOG_TAG, (viewModel?.count).toString())
+            } catch (e: Exception) {
+                if (!TextUtils.isEmpty(e.message)) {
+                    // Here is the error log message
+                    Log.e(LOG_TAG, e.message!!)
+                }
+
+                // Here is the full log message of the error
+                Log.v(LOG_TAG, e.toString())
+
+                // Here is the log message of the error as WTF
+                Log.wtf(LOG_TAG, "What a terrible failure", e)
+            }
         }
     }
 }
